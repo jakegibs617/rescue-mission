e91d73b0808cef268c29b9c4b27937f35e36380b
Jacob <jakegibs617@gmail.com>
2015-09-14 15:05:10 -0400

    min char length answer

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,6 +2,7 @@ class Answer < ActiveRecord::Base
   belongs_to :question
 
   validates :name, presence: true
-  validates :body, presence: true
+  validates :body, presence: true, length: { minimum: 50 }
 
+  default_scope { order('created_at DESC') }
 end
