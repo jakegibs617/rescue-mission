21dd0a792fdb7c482deae04768346ec7cb1cddc5
Jacob <jakegibs617@gmail.com>
2015-09-14 19:48:24 -0400

    css

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,6 @@
 class Question < ActiveRecord::Base
   has_many :answers
 
-
   validates :asker_name, presence: true
   validates :body, presence: true, length: { minimum: 150 }
   validates :title, presence: true, length: { minimum: 40 }
