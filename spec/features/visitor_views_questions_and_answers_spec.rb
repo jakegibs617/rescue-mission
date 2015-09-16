11d93034dc43c848c89e140c1334ba783ecf02aa
Jacob <jakegibs617@gmail.com>
2015-09-16 14:53:27 -0400

    7 examples, 0 failures

--- spec/features/visitor_views_questions_and_answers_spec.rb
+++ spec/features/visitor_views_questions_and_answers_spec.rb
@@ -9,18 +9,25 @@ feature 'visitor views questions and answers', %Q{
   let(:question) { FactoryGirl.build(:question) }
 
   scenario 'ask a question and see it on the root page' do
-
     visit root_path
     click_link "Ask"
-
     fill_in "question_asker_name", :with => question.asker_name
     fill_in "question_title", :with => question.title
     fill_in "question_body", :with => question.body
     click_button "Save Question"
     visit root_path
-
     expect(page).to have_content(question.title)
   end
 
+  scenario 'click on a question and see it in its own page' do
+    visit root_path
+    click_link "Ask"
+    fill_in "question_asker_name", :with => question.asker_name
+    fill_in "question_title", :with => question.title
+    fill_in "question_body", :with => question.body
+    click_button "Save Question"
+
+    expect(page).to have_content(question.body)
+  end
 
 end
