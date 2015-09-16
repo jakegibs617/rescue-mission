74362c46021024d0abb43dd1791699ae1e96a862
Jacob <jakegibs617@gmail.com>
2015-09-16 14:21:28 -0400

    passing

--- spec/features/visitor_views_questions_and_answers_spec.rb
+++ spec/features/visitor_views_questions_and_answers_spec.rb
@@ -2,14 +2,13 @@ require 'rails_helper'
 
 feature 'visitor views questions and answers', %Q{
   As a visitor
-  I want to view a list of questions
-  So that I can see the related answers or answer a question
-  that interests me
+  I want to ask a question
+  And be able to see it with the other questions on the main page
 } do
 
   let(:question) { FactoryGirl.build(:question) }
 
-  scenario 'list of questions on the root page' do
+  scenario 'ask a question and see it on the root page' do
 
     visit root_path
     click_link "Ask"
@@ -18,6 +17,7 @@ feature 'visitor views questions and answers', %Q{
     fill_in "question_title", :with => question.title
     fill_in "question_body", :with => question.body
     click_button "Save Question"
+    visit root_path
 
     expect(page).to have_content(question.title)
   end
